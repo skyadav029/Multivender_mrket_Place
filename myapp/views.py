d5c8fa8aa97f4caca2424d245e4df7b5c516cb7e
skyadav029 <skyadav029@nitjsr.ac.in>
2024-01-11 23:19:35 +0530

    initial commit

---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+     return render(request,'myapp/index.html')
+    # return HttpResponse("this is the home page")
